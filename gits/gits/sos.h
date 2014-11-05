b3f19cfc312006b20f57d58944a21a4af7861850
张晓光 <zhangxiaohikari@zhangxiaokoumatoMacBook-Pro.local>
2014-11-05 15:19:26 +0800

    sos

---
+++ gits/gits/sos.h
@@ -0,0 +1,13 @@
+//
+//  sos.h
+//  gits
+//
+//  Created by 张晓光  on 14/11/5.
+//  Copyright (c) 2014年 张晓光 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sos : UIView
+
+@end
---
+++ gits/gits/sos.m
@@ -0,0 +1,21 @@
+//
+//  sos.m
+//  gits
+//
+//  Created by 张晓光  on 14/11/5.
+//  Copyright (c) 2014年 张晓光 . All rights reserved.
+//
+
+#import "sos.h"
+
+@implementation sos
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
